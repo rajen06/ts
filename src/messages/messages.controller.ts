066c18265299fad152cfcfd73ba585f27a80391a
Raj <mukesh@Laptop334.local>
2024-04-20 23:02:26 +0530

    wip

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+  // constructor(private readonly messagesService: MessagesService) { }
+
+  @Get()
+  listMessages() {
+    return 'listMessages';
+  }
+
+  @Post()
+  createMessage(@Body() body: CreateMessageDto) {
+    return body;
+  }
+
+  @Get('/:id')
+  getMessage(@Param('id') id: string) {
+    return id;
+  }
+}
